c6ede4f22e57400ba16e98af22c8911515b6b25c
Arukado01 <carlosjcortinam@gmail.com>
2026-09-06 16:20:37 -0500

    Engine Config Class

--- Engine/Source/Core/Engine.cpp
+++ Engine/Source/Core/Engine.cpp
@@ -1,6 +1,9 @@
 #include "Core/Engine.h"
+#include "Core/EngineConfig.h"
 
-Engine::Engine() : window_(sf::VideoMode({800, 800}), "Awesome Engine") {
+Engine::Engine()
+    : window_(sf::VideoMode(sf::Vector2u(gConfig.windowSize)),
+              gConfig.windowTitle) {
   window_.setIcon(sf::Image("Content/Textures/Icon.png"));
   window_.setMinimumSize(window_.getSize() / 2u);
 }
---
+++ Engine/Source/Core/EngineConfig.cpp
@@ -0,0 +1,13 @@
+#include "Core/EngineConfig.h"
+#include <cassert>
+#include <fstream>
+#include <nlohmann/json.hpp>
+
+EngineConfig::EngineConfig() {
+  std::ifstream file("Content/Config.json");
+  assert(file);
+
+  nlohmann::json json = nlohmann::json::parse(file);
+  windowTitle = json["windowTitle"];
+  windowSize = {json["windowSize"][0], json["windowSize"][1]};
+}
